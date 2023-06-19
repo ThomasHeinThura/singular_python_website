87f8a0b29cdfbe6efe22528ba18d30e4e4b5a253
ThomasHeinThura <alexzander125.alex@gmail.com>
2023-06-19 12:06:36 +0630

    testing

--- Home_page.py
+++ Home_page.py
@@ -36,6 +36,40 @@ with st.container():
     st.write("##")
     
     st.write("The articles are :")
+    column_one, column_two, column_three, column_four, column_five = st.columns(5)
+    with column_one:
+        st.write("column_one")
+        # st.markdown(
+        #     "![Sonny and Mariel high fiving.](https://content.codecademy.com/courses/learn-cpp/community-challenge/highfive.gif) \
+        #     [![A mushroom-head robot](/assets/images/codey.jpg 'Codey the Codecademy mascot')](https://codecademy.com)"
+        
+        # )
+        st.markdown("[![Foo](assests/ci_cd.jpg)](http://google.com.au/)")
+        pass
+    
+    with column_two:
+        st.write("column_two")
+        st.markdown(
+           "[test](https://github.com/ThomasHeinThura/Testing-automation-ETL-pipeline-and-CI-CD-with-docker)"
+        )
+        st.markdown(
+            "[](assests/repository-open-graph-template.png)"
+        )
+        st.image("assests/repository-open-graph-template.png")
+        pass
+
+    
+    with column_three:
+        st.write("column_three")
+        pass
+    
+    with column_four:
+        st.write("column four")
+        pass
+    
+    with column_five:
+        st.write("column five")
+        pass
     
 
 

2fb5c7740a036e92e3e1939529235562d3a8a19c
ThomasHeinThura <alexzander125.alex@gmail.com>
2023-06-20 12:59:42 +0630

    add photo and test

--- Home_page.py
+++ Home_page.py
@@ -2,6 +2,9 @@
 import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
+from PIL import Image
+
+
 
 
 st.set_page_config(page_title="Multipage App", page_icon=":tada:", layout="wide")
@@ -26,37 +29,43 @@ lottie_article = load_lottieurl(
 
 # ----- Intro SECTION ----- #
 with st.container():
-    st.image("https://avatars.githubusercontent.com/u/29223772?v=4")
-    st.subheader("Hello there, I am Thomas Hein Thura :wave:")
-    st.title("A Freelance Machine Learning Engineer from Myanmar.")
-    st.write("I am passionate about finding ways to use Python")
-    st.write(
+    left_col, right_col = st.columns(2)
+    
+    with right_col:
+        st.image("https://avatars.githubusercontent.com/u/29223772?v=4")
+        st.subheader("Hello there, I am Thomas Hein Thura :wave:")
+        
+    with left_col:
+        st.title("A Freelance Machine Learning Engineer from Myanmar.")
+        st.write("I am passionate about finding ways to use Python")
+        st.write(
+            """
+        Google-certified TensorFlow developer with a strong background in the medical field.
+        Leveraging a broad knowledge of human physiology and pathology, 
+        I am excited to apply my expertise in machine learning and deep learning to develop 
+        innovative solutions that improve the quality of life.
+
+        Email - thomas.h.thura@gmail.com
+
+        𝑺𝑲𝑰𝑳𝑳𝑺
+        - Data engineering with NumPy, Pandas, MLFlow, Prefect, 
+        SQL, SQLite, MongoDB, Prometheus, 
+        - Deployment with docker, Kubernetes, 
+        - Monitoring with EvidentlyAI, Grafana, 
+        - Version and CI/CD with GitHub, Git, AWS(EC2, S3 bucket.), 
+        - Machine Learning modeling with Scikit Learn,
+        - Deep learning modelling(CNN, NLP, Time Series, LLM, Transformer) 
+        with TensorFlow and PyTorch,
+        - Data visualization with Matplotlib and seaborn, python, basic concept of C++, 
+
+        𝑺𝑶𝑭𝑻 𝑺𝑲𝑰𝑳𝑳𝑺 
+        - As a founder of a local charity group, have confidence in communication and 
+        organization skills, with a logical approach to problem-solving, time management, 
+        leadership, and task prioritization skills.
+        - Generalize real-time project issues and seek the best possible solutions.
         """
-    Google-certified TensorFlow developer with a strong background in the medical field.
-    Leveraging a broad knowledge of human physiology and pathology, 
-    I am excited to apply my expertise in machine learning and deep learning to develop 
-    innovative solutions that improve the quality of life.
-
-    Email - thomas.h.thura@gmail.com
-
-    𝑺𝑲𝑰𝑳𝑳𝑺
-    - Data engineering with NumPy, Pandas, MLFlow, Prefect, 
-    SQL, SQLite, MongoDB, Prometheus, 
-    - Deployment with docker, Kubernetes, 
-    - Monitoring with EvidentlyAI, Grafana, 
-    - Version and CI/CD with GitHub, Git, AWS(EC2, S3 bucket.), 
-    - Machine Learning modeling with Scikit Learn,
-    - Deep learning modelling(CNN, NLP, Time Series, LLM, Transformer) 
-    with TensorFlow and PyTorch,
-    - Data visualization with Matplotlib and seaborn, python, basic concept of C++, 
-
-    𝑺𝑶𝑭𝑻 𝑺𝑲𝑰𝑳𝑳𝑺 
-    - As a founder of a local charity group, have confidence in communication and 
-    organization skills, with a logical approach to problem-solving, time management, 
-    leadership, and task prioritization skills.
-    - Generalize real-time project issues and seek the best possible solutions.
-    """
-    )
+        )
+        
     st.write(
         "[Learn More in github >](https://github.com/ThomasHeinThura?tab=repositories)"
     )
@@ -82,32 +91,35 @@ with st.container():
     st.write("##")
 
     column_one, column_two, column_three, column_four = st.columns(4)
-    with column_one:
-        st.markdown(
-            "[Do you really need a data storage server for deep learning](https://medium.com/@thomas.heinthura/do-you-really-need-a-data-storage-server-for-deep-learning-5bb5dc50c02d)"
-        )
-        st.image(
-            "https://miro.medium.com/v2/resize:fit:720/format:webp/1*6llGP0fQ-5kDe_nVBiqoqg.jpeg"
-        )
+    # with column_one:
+    st.markdown(
+        "[Do you really need a data storage server for deep learning](https://medium.com/@thomas.heinthura/do-you-really-need-a-data-storage-server-for-deep-learning-5bb5dc50c02d)"
+    )
+    st.image(
+        "https://miro.medium.com/v2/resize:fit:720/format:webp/1*6llGP0fQ-5kDe_nVBiqoqg.jpeg"
+    )
 
-    with column_two:
-        st.markdown(
-            "[Server for deep learning: A deeper insight](https://medium.com/@thomas.heinthura/server-for-deep-learning-a-deeper-insight-fcb8b7dbb93e)"
-        )
-        st.image(
-            "https://miro.medium.com/v2/resize:fit:720/format:webp/1*oie2fuMbIGM0jZOlx3uD6w.png"
-        )
+    # with column_two:
+    st.markdown(
+        "[Server for deep learning: A deeper insight](https://medium.com/@thomas.heinthura/server-for-deep-learning-a-deeper-insight-fcb8b7dbb93e)"
+    )
+    st.image(
+        "https://miro.medium.com/v2/resize:fit:720/format:webp/1*oie2fuMbIGM0jZOlx3uD6w.png"
+    )
 
-    with column_three:
-        st.markdown(
-            "[Changing career transition is challenging!!!](https://medium.com/@thomas.heinthura/changing-career-transition-is-challenging-93d35f6ac744)"
-        )
-        st.image(
-            "https://miro.medium.com/v2/resize:fit:720/format:webp/1*QKMar94oUNu7bYTfX1xlDQ.jpeg"
-        )
+    # with column_three:
+    st.write(
+        "[![Changing career transition is challenging!!!](https://miro.medium.com/v2/resize:fit:720/format:webp/1*QKMar94oUNu7bYTfX1xlDQ.jpeg)](https://medium.com/@thomas.heinthura/changing-career-transition-is-challenging-93d35f6ac744/)", 
+        # height = 100,
+        # width = 100
+            )
+        
+        # st.image(
+        #     "https://miro.medium.com/v2/resize:fit:720/format:webp/1*QKMar94oUNu7bYTfX1xlDQ.jpeg"
+        # )
 
-    with column_four:
-        st.lottie(lottie_article)
+    # with column_four:
+    st.lottie(lottie_article, height=300, key='article')
 
 
 # ----- Projects Section ----- #
@@ -119,41 +131,47 @@ with st.container():
     st.write("##")
 
     column_one, column_two = st.columns(2)
-    with column_one:
+    image = Image.open('assests/project/personal_website.jpg')
+    # with column_one:
         # --------- #
-        st.markdown(
-            "[Personal website project](https://github.com/ThomasHeinThura/singular_python_website)"
-        )
-        st.image("assests/project/personal_website.jpg")
-        # -------- #
-        st.markdown(
-            "[Text Summarization website](https://github.com/ThomasHeinThura/Text_summarization_website)"
-        )
-        st.image("assests/project/text_summarization.jpg")
+    st.markdown(
+        "[![Personal website project](https://github.com/ThomasHeinThura/singular_python_website/blob/main/assests/project/personal_website.jpg?raw=true)](https://github.com/ThomasHeinThura/singular_python_website)"
+    )
+    
+    # st.markdown("[![Foo](https://github.com/ThomasHeinThura/singular_python_website/blob/main/assests/project/personal_website.jpg?raw=true)](https://github.com/ThomasHeinThura/singular_python_website/)")
+    
+    
+    
+    # st.image("assests/project/personal_website.jpg")
+    # -------- #
+    st.markdown(
+        "[Text Summarization website](https://github.com/ThomasHeinThura/Text_summarization_website)"
+    )
+    st.image("assests/project/text_summarization.jpg")
         # -------- #
 
-        st.markdown(
-            "[Credit Card Fraud Detection](https://github.com/ThomasHeinThura/Credit-Card-Fraud-Detection)"
-        )
-        st.image("assests/project/credit.jpg")
-        # -------- #
+    st.markdown(
+        "[Credit Card Fraud Detection](https://github.com/ThomasHeinThura/Credit-Card-Fraud-Detection)"
+    )
+    st.image("assests/project/credit.jpg")
+    # -------- #
 
-    with column_two:
+    # with column_two:
         # -------- #
-        st.markdown(
-            "[ETL pipeline and CI CD project](https://github.com/ThomasHeinThura/Testing-automation-ETL-pipeline-and-CI-CD-with-docker/)"
-        )
-        st.image("assests/project/ci_cd.jpg")
-        # -------- #
-        st.markdown(
-            "[Tensorflow Projects](https://github.com/ThomasHeinThura/Tensorflow_projects)"
-        )
-        st.image("assests/project/tensorflow.jpg")
+    st.markdown(
+        "[ETL pipeline and CI CD project](https://github.com/ThomasHeinThura/Testing-automation-ETL-pipeline-and-CI-CD-with-docker/)"
+    )
+    st.image("assests/project/ci_cd.jpg")
+    # -------- #
+    st.markdown(
+        "[Tensorflow Projects](https://github.com/ThomasHeinThura/Tensorflow_projects)"
+    )
+    st.image("assests/project/tensorflow.jpg")
 
-        st.markdown(
-            "[My kaggle solution for Parkinson's Freezing of Gait](https://github.com/ThomasHeinThura/Parkinson-s-Freezing-of-Gait-Prediction)"
-        )
-        st.image("assests/project/parkison.jpg")
+    st.markdown(
+        "[My kaggle solution for Parkinson's Freezing of Gait](https://github.com/ThomasHeinThura/Parkinson-s-Freezing-of-Gait-Prediction)"
+    )
+    st.image("assests/project/parkison.jpg")
 
 
 # ----- Education Section ----- #
